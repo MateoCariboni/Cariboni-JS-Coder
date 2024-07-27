d5b27c3722e6830f3458ed2691cd9d79e3bf1e23
MateoCariboni <mateo.cariboni@estudiantes.unahur.edu.ar>
2024-07-27 18:22:31 -0300

    catalogo se muetra al entrar y instrucciones de uso

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ const producto2 = {nombre: "Invicto",precio: 22000,stock: 4, cod:"2"}
 const producto3 = {nombre: "Blue Tequila",precio: 10000,stock: 22, cod:"3"}
 const producto4 = {nombre: "Flowers",precio: 25000,stock: 0, cod:"4"}
 const producto5 = {nombre: "BodySplash",precio: 8000,stock: 3, cod:"5"}
+const producto6 = {nombre: "NightMares",precio: 25000,stock: 0, cod:"6"}
+const producto7 = {nombre: "Difusor",precio: 8000,stock: 3, cod:"7"}
 
-const catalogo = [producto0,producto1,producto2,producto3,producto4,producto5];
+const catalogo = [producto0,producto1,producto2,producto3,producto4,producto5,producto6,producto7];
 
 let carro = [];
 
 let totalCarro = 0;
 
 function mostrarCatalogo(){
+    console.log("Catalogo disponible:")
     for(const producto of catalogo){
         console.log(producto.nombre + "  | Precio: " + producto.precio + " Stock: "+ producto.stock + " CODIGO: " + producto.cod)
     }
+    console.log("Para agregar producto al carro escribir: agregarACarro(codigo del producto)")
+    console.log("Para ver el precio del carro escribir: precioCarro()")
+    console.log("Para finalizar la compra escribir: comprar()")
 }
 
 function precioCarro(){
-    console.log(totalCarro)
+    console.log("Valor del carro: "+totalCarro)
 }
 
-function agregarACarro(){
-    productoAgregar = parseInt(prompt("Ingrese el codigo del producto a agregar:"))
+function agregarACarro(codigoAgregar){
+    productoAgregar = codigoAgregar
     if (catalogo[productoAgregar].stock > 0){
         carro.push(catalogo[productoAgregar])
         catalogo[productoAgregar].stock--
@@ -33,10 +39,13 @@ function agregarACarro(){
     for (i of carro){
         precioTotal += i.precio
     }
-    totalCarro += precioTotal 
+    totalCarro += precioTotal
+    console.log("Haz agregado el articulo COD:"+codigoAgregar+". Valor del carro: "+totalCarro)
 }
 
 function comprar(){
     nombreComprador = prompt("Ingresa su nombre:")
     alert("Va a realizar un pago de $" + totalCarro + " pesos")
 }
+
+mostrarCatalogo()
